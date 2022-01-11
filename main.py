75e3027f39cff48076729b944655abf9e4ac9f71
Manu Datta <85798111+WizardManu@users.noreply.github.com>
2022-01-11 23:00:00 +0000

    Added Splits

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ class hand():
 
 def PlayBlackJack():
 
+  ShuffledDeck = deck()
   DealerHand = hand()
   YourHand = hand()
 
@@ -86,60 +87,81 @@ def PlayBlackJack():
     print("Dealer Has BlackJack, You Lose")
     return(-1)
 
+  rounds = 1
+  OnFirstHand = False
 
-  answer = input('Double Down?')
-  DoubledDown = False
-  if answer == 'Yes' or answer == 'yes':
-    DoubledDown = True
-    ShuffledDeck.draw(YourHand)
-    print('Your New Hand is', YourHand.cards)
-    playing = False
+  if YourHand.cards[0][0] == YourHand.cards[1][0]:
+    if input('Do You Want to Split?') == 'yes':
+      OnFirstHand = True
+      rounds = 2
+      SecondHand = hand()
+      SecondHand.cards.insert(0,YourHand.cards[1])
+      del(YourHand.cards[1])
+      ShuffledDeck.draw(YourHand)
+      ShuffledDeck.draw(SecondHand)
+      print('Your First Hand is', YourHand.cards)
+      print('Your Second Hand is', SecondHand.cards)
 
-
-  while playing:
-    HitOrStand = input("Hit or Stand?")
-    if HitOrStand == 'Hit':
+  TruePointChange = 0
+  for round in range(0,rounds):
+    answer = input('Double Down?')
+    DoubledDown = False
+    if answer == 'Yes' or answer == 'yes':
+      DoubledDown = True
       ShuffledDeck.draw(YourHand)
       print('Your New Hand is', YourHand.cards)
-    elif HitOrStand == 'Stand':
       playing = False
-    else:
-      print('Thats not a valid command')
+
+
+    while playing:
+      HitOrStand = input("Hit or Stand?")
+      if HitOrStand == 'Hit':
+        ShuffledDeck.draw(YourHand)
+        print('Your New Hand is', YourHand.cards)
+      elif HitOrStand == 'Stand':
+        playing = False
+      else:
+        print('Thats not a valid command')
+      if YourHand.count()[0] == True:
+        playing = False
+      if YourHand.count()[2] == True:
+        print('Your Count Is',YourHand.count()[1],'/',YourHand.count()[1] - 10)
+      else:
+        print('Your Count Is',YourHand.count()[1])
+      print('')
+    while DealerHand.count()[1] < 17:
+      ShuffledDeck.draw(DealerHand)
+      print('Dealers New Hand is', DealerHand.cards)
+    print('Dealers Has', DealerHand.cards)
+    print('You Have', YourHand.cards)
     if YourHand.count()[0] == True:
-      playing = False
-    if YourHand.count()[2] == True:
-      print('Your Count Is',YourHand.count()[1],'/',YourHand.count()[1] - 10)
-    else:
-      print('Your Count Is',YourHand.count()[1])
-    print('')
-  while DealerHand.count()[1] < 17:
-    ShuffledDeck.draw(DealerHand)
-    print('Dealers New Hand is', DealerHand.cards)
-  print('Dealers Has', DealerHand.cards)
-  print('You Have', YourHand.cards)
-  if YourHand.count()[0] == True:
-    print('Bust, You Lose')
-    PointChange = -1
-  elif DealerHand.count()[0] == True:
-    print('Dealer Busts, You Win')
-    PointChange = 1
-  elif DealerHand.count()[1] > YourHand.count()[1]:
-    print('Dealer has Higher Count, You Lose')
-    PointChange =  -1
-  elif DealerHand.count()[1] < YourHand.count()[1]:
-    print('You Have Higher Count, You Win')
-    PointChange = 1
-  elif DealerHand.count()[1] == YourHand.count()[1]:
-    print('Push, You Tie')
-    PointChange = 0
-  if DoubledDown:
-    PointChange = PointChange * 2
-  return(PointChange)
+      print('Bust, You Lose')
+      PointChange = -1
+    elif DealerHand.count()[0] == True:
+      print('Dealer Busts, You Win')
+      PointChange = 1
+    elif DealerHand.count()[1] > YourHand.count()[1]:
+      print('Dealer has Higher Count, You Lose')
+      PointChange =  -1
+    elif DealerHand.count()[1] < YourHand.count()[1]:
+      print('You Have Higher Count, You Win')
+      PointChange = 1
+    elif DealerHand.count()[1] == YourHand.count()[1]:
+      print('Push, You Tie')
+      PointChange = 0
+    if DoubledDown:
+      PointChange = PointChange * 2
+    if OnFirstHand:
+      YourHand.cards = SecondHand.cards
+      OnFirstHand = False
+      print('You Are Now On Your Second Hand')
+      print('')
+    TruePointChange += PointChange  
+  return(TruePointChange)
 
 InGame = True
 points = 0
 while InGame:
-  ShuffledDeck = deck()
   os.system('cls' if os.name == 'nt' else 'clear')
   print('You Have', points)
   points += PlayBlackJack()
